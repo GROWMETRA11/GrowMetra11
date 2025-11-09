17e96cdf23d968e8f048eb037140ccaa19c42669
J0ker11 <primetech444@gmail.com>
2025-11-09 18:34:30 +0100

    fix: error on icons

---
+++ src/Components/iconComponent/BagIcon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BagIcon = () => {
+  return (
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 4C0 1.79086 1.79086 0 4 0H28C30.2091 0 32 1.79086 32 4V28C32 30.2091 30.2091 32 28 32H4C1.79086 32 0 30.2091 0 28V4Z" fill="#DFE9F2" fill-opacity="0.5"/>
+<path d="M23 11H21C21 8.2 18.8 6 16 6C13.2 6 11 8.2 11 11H9C7.9 11 7 11.9 7 13V25C7 26.1 7.9 27 9 27H23C24.1 27 25 26.1 25 25V13C25 11.9 24.1 11 23 11ZM16 8C17.7 8 19 9.3 19 11H13C13 9.3 14.3 8 16 8ZM23 25H9V13H23V25ZM16 17C14.3 17 13 15.7 13 14H11C11 16.8 13.2 19 16 19C18.8 19 21 16.8 21 14H19C19 15.7 17.7 17 16 17Z" fill="#097FDD"/>
+</svg>
+
+  )
+}
+
+export default BagIcon
