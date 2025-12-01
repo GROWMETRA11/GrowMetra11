746b593170e2a9291aab4b5bf5997eeb5b978296
J0ker11 <primetech444@gmail.com>
2025-12-01 22:23:59 +0100

    feat: Added wallet section on dashboard

--- src/pages/dashboard/famers/screen/Wallet.jsx
+++ src/pages/dashboard/famers/screen/Wallet.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import DashboardLayout from '../components/DashboardLayout'
 
 const Wallet = () => {
+
+  const walletPrice = [
+    {
+      icon: ,
+      text: "",
+      balance: ,
+      hidden: ,
+    },
+  ]
+
+
   return (
        <DashboardLayout>
-      <p>This is order </p>
+      <h2 className='font-normal text-[32px] '>My Wallet</h2>
+      <p className='font-normal text-base '>Manage your funds and purchases securely</p>
+
+
     </DashboardLayout>
   )
 }
