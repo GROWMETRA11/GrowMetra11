67f6109f0f63a9ec664137a699c39181a6b240a1
J0ker11 <primetech444@gmail.com>
2025-12-02 02:14:21 +0100

    Feat: Added wallet section in dashboard

---
+++ src/Components/iconComponent/WidthdrawFundsIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WidthdrawFundsIcon = () => {
+  return (
+    <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4.66602 9.32867V8.16667C4.66602 7.23841 5.03476 6.34817 5.69114 5.69179C6.34752 5.03541 7.23776 4.66667 8.16602 4.66667H19.8327C20.7609 4.66667 21.6512 5.03541 22.3076 5.69179C22.9639 6.34817 23.3327 7.23841 23.3327 8.16667V9.33333M13.9993 22.75V9.91667M13.9993 9.91667L18.0827 14M13.9993 9.91667L9.91602 14" stroke="#4A4A4A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+  )
+}
+
+export default WidthdrawFundsIcon
