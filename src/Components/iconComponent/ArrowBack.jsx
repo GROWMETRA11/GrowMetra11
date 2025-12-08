ea5f2b564ca7a26038fb3397dab00401ac859c66
J0ker11 <primetech444@gmail.com>
2025-12-08 23:18:16 +0100

    feat: Added add funds page

---
+++ src/Components/iconComponent/ArrowBack.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ArrowBack = () => {
+  return (
+    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10 24H38M10 24L22 12M10 24L22 36" stroke="black" stroke-width="2.66667" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+  )
+}
+
+export default ArrowBack
